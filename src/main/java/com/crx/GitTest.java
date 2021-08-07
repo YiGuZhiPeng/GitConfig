0f8bb413922b2b3a5512068d28ef312ada0a543e
gupeng <gupeng2022@163.com>
2021-08-07 17:06:26 +0800

    second commit

--- src/main/java/com/crx/GitTest.java
+++ src/main/java/com/crx/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         int i = 1;
         System.out.println("hello git");
+        System.out.println("hello github");
     }
 }
