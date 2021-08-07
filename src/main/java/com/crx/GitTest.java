58974a76ff6fbc96b14c098080539b0b67f1e464
gupeng <gupeng2022@163.com>
2021-08-07 17:18:20 +0800

    hot-fix commit1

--- src/main/java/com/crx/GitTest.java
+++ src/main/java/com/crx/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello gitAll");
+        int y = 2;
+        System.out.println("hello gitAllFix");
     }
 }
